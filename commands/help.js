b7d330d3938645c1c1d32505f91e35b7b96fb636
stargazingx13 <sofiapalmira2001@gmail.com>
2019-04-02 17:44:41 -0400

    Command changes.

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 module.exports.run = (bot, message, args) => {
 message.channel.send('**Info Commands**```help, m-help, servericon, serverinfo, usericon, userinfo```**Math Commands**```add, subtract, multiply, divide```**Other Commands**```cat, coin, dog, roll, snap, tarot```');
 if (message.guild.id === '450088547857465349') {
-    message.channel.send("**Bunker Commands** ```nsfw, kinkshame, mod-me, sad, say, thanosquote, waifu```");
+    message.channel.send("**Bunker Commands** ```kinkshame, mod-me, sad, say, thanosquote, waifu```");
 }
 };
--- commands/sad.js
+++ commands/sad.js
@@ -6,9 +6,18 @@ i[0] = `This is so sad, can we cancel Timo's killing game?`;
 i[1] = `This is so sad, Botgazing, play Despacito.`;
 i[2] = `This is so sad, can we record another podcast?`;
 i[3] = `This is so sad, can we make Mak the killer?`;
-i[4] = `This is so sad, can we find Franklin's Wii U?`;
+i[4] = `This is so sad, can we find Mfan's Wii U?`;
 i[5] = `This is so sad, F in chat.`;
-i[6] = `This is so sad, can we buy Star some iTunes cards for gacha whaling?`;
+i[6] = `This is so sad, can we buy Sofia some iTunes cards for weeb pngs?`;
+i[7] = `This is so sad, can we cancel Bunker Bliss?`;
+i[8] = `This is so sad, come to voice chat.`;
+i[9] = `This is so sad, can we demote Aidan?`;
+i[10] = `This is so sad, can we make Sofia more active?`;
+i[11] = `This is so sad, can we wake Mfan up?`;
+i[12] = `This is so sad, delete Math.`;
+i[13] = `This is so sad, can we finish Pirate Killing Games's maps?`;
+i[14] = `This is so sad, can we drop Aidan's DS?`;
+i[15] = `This is so sad, host a VC party.`;
 
 var j = Math.round(Math.random() * 6);
 
