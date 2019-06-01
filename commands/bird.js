2f9ced7c967ecd1748abd0da0b2a205cd377c35e
stargazingx13 <sofiapalmira2001@gmail.com>
2019-06-01 16:24:47 -0400

    h

--- commands/bird.js
+++ commands/bird.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args) => {
     const superagent = require ("superagent");
 
     let {body} = await superagent
-    .get(`http://random.birb.pw/tweet.json/`);
+    .get(`http://random.birb.pw/tweet.json`);
     if (!{body}) return message.channel.send("File not generated. Please try again!");
 
     const embed = new Discord.RichEmbed()
