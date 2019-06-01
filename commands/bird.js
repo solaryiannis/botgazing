ab67049c7b7f14db9ca1aa8c91abb2ef599937cd
stargazingx13 <sofiapalmira2001@gmail.com>
2019-06-01 16:12:39 -0400

    Finally updated a few things.

---
+++ commands/bird.js
@@ -0,0 +1,15 @@
+module.exports.run = async (bot, message, args) => {
+    const Discord = require('discord.js');
+    const superagent = require ("superagent");
+
+    let {body} = await superagent
+    .get(`http://random.birb.pw/tweet.json/`);
+    if (!{body}) return message.channel.send("File not generated. Please try again!");
+
+    const embed = new Discord.RichEmbed()
+    .setTitle("%bird")
+    .setColor(0x00000)
+    .setImage(body.file)
+ 
+  message.channel.send({embed});
+};
