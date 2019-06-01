6d74046fe857c18be4639fa61af14c08df1eaf04
stargazingx13 <sofiapalmira2001@gmail.com>
2019-06-01 16:28:58 -0400

    h

--- commands/bird.js
+++ commands/bird.js
@@ -3,13 +3,13 @@ module.exports.run = async (bot, message, args) => {
     const superagent = require ("superagent");
 
     let {body} = await superagent
-    .get(`http://random.birb.pw/tweet.json`);
+    .get(`http://shibe.online/api/birds`);
     if (!{body}) return message.channel.send("File not generated. Please try again!");
 
     const embed = new Discord.RichEmbed()
     .setTitle("%bird")
     .setColor(0x00000)
-    .setImage(`https://random.birb.pw/img/${body.file}`)
+    .setImage(body.0)
  
   message.channel.send({embed});
 };
