ab5ab134958bbab2861a16ee62c1ca300745e582
stargazingx13 <sofiapalmira2001@gmail.com>
2019-08-08 14:25:57 -0400

    Edited some files.

--- commands/rolelist.js
+++ commands/rolelist.js
@@ -1,3 +1,17 @@
 module.exports.run = (bot, message, args) => {
-    message.channel.send(message.guild.roles.map(role => role.name).join(", "));
+    message.channel.send({embed: {
+        color: 0x000000,
+        author: {
+          name: `Role List for ${message.guild.name} (${message.guild.id})`,
+          icon_url: message.guild.iconURL
+        },
+        title: "Roles",
+        description: message.guild.roles.map(role => role.name).join(", "),
+        timestamp: new Date(),
+        footer: {
+          icon_url: bot.user.avatarURL,
+          text: `I'm ${bot.user.tag}!`
+        }
+      }
+    });
     };
