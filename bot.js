99688ea2e7aad70b3118ca5063c7299b7f919372
stargazingx13 <sofiapalmira2001@gmail.com>
2019-08-09 09:19:54 -0400

    h

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ const commandFiles = fs.readdirSync('./commands').filter(f => f.endsWith('.js'))
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	bot.commands.set(command.name, command);
+	bot.commands.set(command.n, command);
 }
 
 bot.on ("message", async message => {
