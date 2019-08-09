bd470361052e80d3638c41e1889644d8c5ee13b7
stargazingx13 <sofiapalmira2001@gmail.com>
2019-08-09 11:36:51 -0400

    Finally.

---
+++ commands/roleinfo.js
@@ -0,0 +1,57 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	n: 'roleinfo',
+	a: ['roleinfo', 'ri', 'role'],
+    d: 'If you need to know what role does what.',
+    u: '%roleinfo <role name>',
+    async execute(client, message, args) {
+
+        if (!args) return message.channel.send("Please type the name of a role.");
+        let findRole = message.guild.roles.find(r => r.name === args.join(" "));
+
+        if (!findRole) return message.channel.send("I couldn't find that role...");
+
+        message.channel.send({embed: {
+            color: findRole.colour,
+            author: {
+              name: `${findRole.name} (${findRole.id})`,
+              icon_url: message.author.avatarURL
+            },
+            title: "Hex Colour",
+            description: findRole.hexColor,
+            fields: [
+            {
+                name: "Mentionable",
+                value: findRole.mentionable
+            },
+            {
+                name: "Hoisted",
+                value: findRole.hoist
+            },
+            {
+                name: "Role Position",
+                value: findRole.calculatedPosition
+            },
+            {
+                name: "Mentionable",
+                value: findRole.mentionable
+            },
+            {
+                name: "Permissions",
+                value: findRole.permissions
+            },
+            {
+                name: "Created At",
+                value: new Date(findRole.createdTimestamp).toGMTString()
+            },
+            ],
+            timestamp: new Date(),
+            footer: {
+              icon_url: client.user.avatarURL,
+              text: `I'm ${client.user.tag}!`
+            }
+          }
+        });
+  },
+};
