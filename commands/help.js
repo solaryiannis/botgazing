0ff5fc7d8e04b5949cb861bae8d6ed398490deb9
stargazingx13 <sofiapalmira2001@gmail.com>
2019-08-09 11:08:59 -0400

    Finally.

--- commands/help.js
+++ commands/help.js
@@ -8,14 +8,14 @@ module.exports = {
     async execute(client, message, args) {
         const { commands } = message.client;
         if (!args.length) {
-            message.channel.send(`**Commands List:**\n\`\`\`${commands.map(command => command.n).join(', ')}\`\`\`\nFor help with a specific command, type \`$help <command>\`!`);
+            return message.channel.send(`**Commands List:**\n\`\`\`${commands.map(command => command.n).join(', ')}\`\`\`\nFor help with a specific command, type \`$help <command>\`!`);
         }
 
         const name = args[0];
         const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
         if (!command) {
-	        return message.reply('That\'s not a valid command...');
+	        return message.channel.send(`**Commands List:**\n\`\`\`${commands.map(command => command.n).join(', ')}\`\`\`\nFor help with a specific command, type \`$help <command>\`!`);
         }
 
         message.channel.send({embed: {
