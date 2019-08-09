a87b118c71424d2054238c1b2a1d6cf6a64fd583
stargazingx13 <sofiapalmira2001@gmail.com>
2019-08-09 19:17:40 -0400

    Let's try something new!

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -8,27 +8,28 @@ module.exports = {
 	b: false,
 	async execute(client, message, args) {
     var userMember;
-
-    if (message.mentions.users.size === 0) {
-        userMember = message.author;
-        memberUser = message.member;
+    const member = message.mentions.members.first();
+    if !member {
+      member = message.guild.members.get(args[0]);
+      if (!member)
+      {
+        if (message.guild.members.find(args[0])) return message.channel.send ("Couldn't find the user...");
+        member = message.guild.members.find(args[0]);
+      }
     }
-    else {
-        userMember = message.mentions.users.first();
-        memberUser = message.mentions.members.first();
-    }
+    const user = member.user;
     
     const embed = new Discord.RichEmbed()
-    .setColor(memberUser.displayColor)
+    .setColor(member.displayColor)
     .setTitle(`%userinfo`)
-  	.setThumbnail(userMember.avatarURL)
-  	.addField('User', `${userMember.tag} (${userMember.id})`)
+  	.setThumbnail(user.avatarURL)
+  	.addField('User', `${user.tag} (${user.id})`)
   	.addBlankField()
-  	.addField('Bot User', userMember.bot, true)
-    .addField('Presence', memberUser.presence.status, true)
-    .addField('Account Created', new Date(userMember.createdTimestamp).toGMTString())
-    .addField('Server Joined', new Date(memberUser.joinedTimestamp).toGMTString())
-    .addField(`Roles (${memberUser.roles.size})`, memberUser.roles.map(role => role.name).join(", "))
+  	.addField('Bot User', user.bot, true)
+    .addField('Presence', member.presence.status, true)
+    .addField('Account Created', new Date(user.createdTimestamp).toGMTString())
+    .addField('Server Joined', new Date(member.joinedTimestamp).toGMTString())
+    .addField(`Roles (${member.roles.size})`, member.roles.map(role => role.name).join(", "))
   	.setTimestamp()
   	.setFooter(`I'm ${client.user.tag}!`, client.user.avatarURL);
     message.channel.send({embed});
