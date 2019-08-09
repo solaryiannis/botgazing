7570fe38ce4b16ea9163eb4a214c478bea6b1b8e
stargazingx13 <sofiapalmira2001@gmail.com>
2019-08-09 09:38:07 -0400

    h

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const fs = require('fs');
 
-bot.on ("ready", () => {
+bot.once ("ready", () => {
   console.log(`spiderman 2 pizza theme`);
-  bot.user.setActivity('%help');
+  bot.user.setActivity(`${process.env.PREFIX}help`);
   });
 
-const prefix = "%";
+const prefix = process.env.PREFIX;
 
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
@@ -16,19 +16,22 @@ const commandFiles = fs.readdirSync('./commands').filter(f => f.endsWith('.js'))
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	bot.commands.set(command.n, command);
+  bot.commands.set(command.n, command);
+  console.log(`command.n: ${command.n}. ${file} successfully loaded`);
 }
 
 bot.on ("message", async message => {
 if (message.author.bot || !message.content.startsWith(prefix)) return;
 
-let messageArray = message.content.split(" ");
-let cmd = messageArray[0].toLowerCase();
-let args = messageArray.slice(1);
+const args = message.content.slice(prefix.length).split(/ +/);
+const cmd = args.shift().toLowerCase();
+
+console.log(`args: ${args}. command: ${cmd}. ${cmd} successfully loaded`);
 
 if (!bot.commands.has(cmd)) return;
 try {
-bot.commands.get(command).execute(message, args);
+bot.commands.get(cmd).execute(message, args);
+console.log(`trying to execute ${cmd}`);
 } catch (e) {
 console.log(e);
 }
