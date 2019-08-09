4bf1f8debb5002ac6d733d62333041056619e862
stargazingx13 <sofiapalmira2001@gmail.com>
2019-08-09 09:43:22 -0400

    h

--- bot.js
+++ bot.js
@@ -17,21 +17,19 @@ const commandFiles = fs.readdirSync('./commands').filter(f => f.endsWith('.js'))
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
   bot.commands.set(command.n, command);
-  console.log(`command.n: ${command.n}. ${file} successfully loaded`);
 }
 
 bot.on ("message", async message => {
 if (message.author.bot || !message.content.startsWith(prefix)) return;
 
 const args = message.content.slice(prefix.length).split(/ +/);
-const cmd = args.shift().toLowerCase();
+const cmdName = args.shift().toLowerCase();
 
-console.log(`args: ${args}. command: ${cmd}. ${cmd} successfully loaded`);
-
-if (!bot.commands.has(cmd)) return;
+if (!bot.commands.has(cmdName)) return;
+const cmd = client.commands.get(commandName) || client.commands.find(cmd => cmd.a && cmd.a.includes(cmdName));
 try {
 bot.commands.get(cmd).execute(message, args);
-console.log(`trying to execute ${cmd}`);
+console.log(`${cmd} trying to execute.`);
 } catch (e) {
 console.log(e);
 }
