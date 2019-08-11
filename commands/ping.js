7a3f275b75d2b9dacc2073c9aa5bf1897656bf68
stargazingx13 <sofiapalmira2001@gmail.com>
2019-08-11 17:16:39 -0400

    h

---
+++ commands/ping.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	n: 'ping',
+	a: ['ping', 'p'],
+    d: 'Ping pong!',
+    u: '%ping',
+	b: false,
+	async execute(client, message, args) {
+    message.channel.send(`Pong! (currently at ${client.pings[0]})`);
+},
+};
--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,12 +1,44 @@
 const Discord = require('discord.js');
 
 module.exports = {
-	n: 'ping',
-	a: ['ping', 'p'],
-    d: 'Ping pong!',
-    u: '%ping',
+	n: 'userinfo',
+	a: ['userinfo', 'uinfo'],
+    d: 'Gets the mentioned user\'s (or your) info.',
+    u: '%userinfo or %userinfo <user>',
 	b: false,
 	async execute(client, message, args) {
-    message.channel.send(`Pong! (currently at ${client.pings[0]})`);
+    var member;
+    var user;
+    member = message.mentions.members.first();
+    if (!member) {
+      member = message.guild.members.get(args[0]);
+      if (!member)
+      {
+        member = message.guild.members.find(m => m.user.username === args.join(" "));
+        if (!member) {
+          member = message.guild.members.find(m => m.displayName === args.join(" "));
+          if (!member) {
+              member = message.guild.members.find(m => m.user.tag === args.join(" "));
+              if (!member) member = message.member;
+          }  
+        }
+      }
+    }
+    user = member.user;
+    
+    const embed = new Discord.RichEmbed()
+    .setColor(member.displayColor)
+    .setTitle(`%userinfo`)
+  	.setThumbnail(user.avatarURL)
+  	.addField('User', `${user.tag} (${user.id})`)
+  	.addBlankField()
+  	.addField('Bot User', user.bot, true)
+    .addField('Presence', member.presence.status, true)
+    .addField('Account Created', new Date(user.createdTimestamp).toGMTString())
+    .addField('Server Joined', new Date(member.joinedTimestamp).toGMTString())
+    .addField(`Roles (${member.roles.size})`, member.roles.map(role => role.name).join(", "))
+  	.setTimestamp()
+  	.setFooter(`I'm ${client.user.tag}!`, client.user.avatarURL);
+    message.channel.send({embed});
 },
 };
